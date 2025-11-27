4276665d558899c69cca4eb3f29e59c151e1bea6
ShinnyCCT <chitin7.chen@gmail.com>
2025-11-27 17:38:26 +0800

    Add files via upload

--- index.tsx
+++ index.tsx
@@ -13,15 +13,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-if ('serviceWorker' in navigator) {
-    // 註冊 /sw.js 檔案作為 Service Worker
-    window.addEventListener('load', () => {
-        //navigator.serviceWorker.register('/sw.js', { scope: '/' })
-            .then(registration => {
-                console.log('Service Worker 註冊成功, 範圍:', registration.scope);
-            })
-            .catch(error => {
-                console.error('Service Worker 註冊失敗:', error);
-            });
-    });
-}
