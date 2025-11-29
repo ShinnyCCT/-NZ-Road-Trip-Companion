6b5054bcd84a6416031581a8db31eaf078f62c0d
ShinnyCCT <chitin7.chen@gmail.com>
2025-11-30 00:08:59 +0800

    解決Tailwind CSS 的一個警告

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./index.html",
-    "./**/*.{js,ts,jsx,tsx}", // 掃描根目錄和所有子目錄下的所有 React 檔案
+    "./*.{js,ts,jsx,tsx}", // 掃描根目錄和所有子目錄下的所有 React 檔案
   ],
   theme: {
     extend: {},
